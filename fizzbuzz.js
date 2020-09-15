d2b6fafb2a1dfcaf30a78cdb00cff51d5e650aad
Yotam Sandbank <yotamsandbank@gmail.com>
2020-09-15 11:13:39 +0300

    expressjs file

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
+app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
